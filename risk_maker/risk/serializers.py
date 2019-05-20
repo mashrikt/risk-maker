5c79716ce9c90997e2bc41497907f41c1b2fd5ad
Tahzib Mashrik <mashrikt@gmail.com>
2019-05-21 03:35:33 +0600

    Handle case when number is sent as string

--- risk_maker/risk/serializers.py
+++ risk_maker/risk/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer, ValidationError
 
+from risk_maker.risk.config import FieldType
 from risk_maker.risk.models import RiskField, RiskType, Risk
 from risk_maker.risk.utils import is_correct_field_type
 
@@ -34,9 +35,11 @@ class RiskSerializer(ModelSerializer):
         for risk_field in risk_fields:
             name = risk_field.name
             val = data.get(name, None)
+            field_type = risk_field.field_type
+            val = int(val) if field_type == FieldType.NUMBER and type(val) == str and val.isdigit() else val
             if risk_field.is_required and val is None:
                 error_message[name] = ['This field may not be null.']
-            elif val and not is_correct_field_type(risk_field.field_type, val, risk_field.choices):
+            elif val and not is_correct_field_type(field_type, val, risk_field.choices):
                 error_message[name] = f'Please enter a valid {risk_field.get_field_type_display()}'
             elif val is not None:
                 processed_data[name] = val
